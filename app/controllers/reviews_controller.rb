992c90d40d30ddc8ece7cf821054c82fcc1b4f54
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-09-29 19:52:56 +0300

    Nested routes codealong

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,9 +2,15 @@ class ReviewsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
+    if params[:dog_house_id]
+			dog_house = DogHouse.find(params[:dog_house_id])
+			reviews = dog_house.reviews
+		else
     reviews = Review.all
+    end
     render json: reviews, include: :dog_house
   end
+  
 
   def show
     review = Review.find(params[:id])
@@ -16,14 +22,14 @@ class ReviewsController < ApplicationController
     render json: review, status: :created
   end
 
-  private
+    private
 
-  def render_not_found_response
-    render json: { error: "Review not found" }, status: :not_found
-  end
+    def render_not_found_response
+      render json: { error: "Review not found" }, status: :not_found
+    end
 
-  def review_params
-    params.permit(:username, :comment, :rating)
-  end
+    def review_params
+      params.permit(:username, :comment, :rating)
+    end
 
 end
